b0a35597c0ad06b198c8a35cbf4b94a825343504
Neeraj <neerajchandekar@gmail.com>
2025-06-01 18:08:33 -0700

    Added NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import './NavBar.css'; // Import the CSS file
 
-const NavBar = () => (
-  <nav className="navbar">
-    <img src="/logo.png" alt="Logo" />
-    <ul>
-      <li><a href="#home">Home</a></li>
-      <li><a href="#details">Program Details</a></li>
-      <li><a href="#calendar">Calendar</a></li>
-      <li><a href="#about">About Us</a></li>
-      <li><a href="#contact">Contact Us</a></li>
-      <li><a href="#privacy">Privacy Policy</a></li>
-    </ul>
-  </nav>
-);
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-logo">MyBusiness</div>
+      <ul className="nav-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#details">Program Details</a></li>
+        <li><a href="#about">About Us</a></li>
+        <li><a href="#events">Events</a></li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
